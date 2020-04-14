89e70928e3c9dc717e5c9c0081e263fabcfc328b
tanujkher <tanujkher15@gmail.com>
2020-04-14 15:49:48 +0530

    Stress test

--- MaxPairwiseProduct.java
+++ MaxPairwiseProduct.java
@@ -1,16 +1,37 @@
-import java.util.*;
-
 public class MaxPairwiseProduct{
     public static void main(String[] args) {
-        Scanner scn = new Scanner(System.in);
-        int n = scn.nextInt();
-        long[] arr = new long[n];
-        for(int i = 0 ; i <= n - 1; i++){
-            arr[i] = scn.nextLong();
+        while(true){
+            int n = (int)(Math.random() * 10) + 2;
+            long[] arr = new long[n];
+            for(int i = 0; i <= arr.length - 1; i++){
+                arr[i] = (int)(Math.random() * 100);
+            }
+            for(long val : arr){
+                System.out.print(val + " ");
+            }
+            System.out.println();
+            long na = Naive(arr);
+            long mod = FindMaxProduct(arr);
+            System.out.println(na + " " + mod);
+            if(na != mod){
+                System.out.println("Wrong answer");
+                break;
+            }else{
+                System.out.println("OK");
+            }
         }
-        System.out.println(FindMaxProduct(arr));
-        scn.close();
     }
+
+    private static long Naive(long[] arr){
+        long result = 0;
+        for(int i = 0; i <= arr.length - 1; i++){
+            for(int j = i + 1; j <= arr.length - 1; j++){
+                result = Math.max(result, arr[i] * arr[j]);
+            }
+        }
+        return result;
+    }
+
     private static long FindMaxProduct(long[] arr){
         long max1 = Long.MIN_VALUE;
         long max2 = Long.MIN_VALUE;
