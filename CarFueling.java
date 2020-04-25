1d74e918c5b351cf052d7899bbc30d8a80c02f99
tanujkher <tanujkher15@gmail.com>
2020-04-25 16:47:54 +0530

    Car Fueling problem done must look

--- CarFueling.java
+++ CarFueling.java
@@ -1,34 +1,43 @@
 import java.util.*;
 
+// 950
+// 400
+// 4
+// 200 375 550 750
+
 public class CarFueling {
-    private static int computeMinRefills(int dist, int tank, int[] stops) {
-        int fills = 0;
-        int last_fill = 0;
-        for(int i = 0; i <= stops.length - 1; i++){
-            int next = i + 1 <= stops.length - 1 ? stops[i + 1] : -1;
-            if(next == -1){
-                if(last_fill + tank >= dist){
-                    return fills;
-                }
-            }
-            if(tank - next < 0){
-                last_fill = stops[i];
-                fills = fills + 1;
-            }
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int dist = scn.nextInt();
+        int cap = scn.nextInt();
+        int n = scn.nextInt();
+        int[] arr = new int[n + 2];
+        arr[0] = 0;
+        for(int i = 1; i <= n; i++){
+            arr[i] = scn.nextInt();
         }
-        return -1;
+        arr[arr.length - 1] = dist;
+        System.out.println(minFill(dist, cap, arr));
     }
 
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int dist = scanner.nextInt();
-        int tank = scanner.nextInt();
-        int n = scanner.nextInt();
-        int stops[] = new int[n + 1];
-        for (int i = 0; i < n; i++) {
-            stops[i] = scanner.nextInt();
+    private static int minFill(int dist, int cap, int[] arr) {
+        int refil = 0;
+        for(int i = 0; i <= arr.length - 1;){
+            int curr = i;
+            for(; i + 1 <= arr.length - 1 && arr[i + 1] - arr[curr] <= cap;){
+                i++;
+            }
+            if(i == arr.length - 1){
+                return refil;
+            }
+            if(i == curr){
+                return -1;
+            }
+            if(i <= arr.length - 1){
+                refil++;
+                // System.out.println("refil at " + i + "  no " + refil);
+            }
         }
-        stops[stops.length - 1] = dist;
-        System.out.println(computeMinRefills(dist, tank, stops));
+        return refil;
     }
 }
